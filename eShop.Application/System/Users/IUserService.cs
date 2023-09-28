92244a2b6c7c7f74b6c0e301b64fc4fce83a1847
tuannucezzz <vantuan100301@gmail.com>
2023-09-28 12:21:02 +0700

    Login and Register

---
+++ eShop.Application/System/Users/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using eShop.ViewModels.System.Users;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eShop.Application.System.Users
+{
+    public interface IUserService
+    {
+        Task<string> Authenticate(LoginRequest request);
+        Task<bool> Register(RegisterRequest request);
+    }
+}
--- eShop.BackendApi/Startup.cs
+++ eShop.BackendApi/Startup.cs
@@ -1,10 +1,13 @@
 using eShop.Application.Catalog.Products;
 using eShop.Application.Common;
+using eShop.Application.System.Users;
 using eShop.Data.EF;
+using eShop.Data.Entities;
 using eShop.Utilities.Constant;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -31,10 +34,18 @@ namespace eShop.BackendApi
         {
             services.AddDbContext<EShopDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString(SystemConstants.MainConnectionString)));
 
+            services.AddIdentity<AppUser, AppRole>()
+                .AddEntityFrameworkStores<EShopDbContext>()
+                .AddDefaultTokenProviders();
+
             //Declare DI
             services.AddTransient<IPublicProductService, PublicProductService>();
             services.AddTransient<IManageProductService, ManageProductService>();
             services.AddTransient<IStorageService, FileStorageService>();
+            services.AddTransient<UserManager<AppUser>, UserManager<AppUser>>();
+            services.AddTransient<SignInManager<AppUser>, SignInManager<AppUser>>();
+            services.AddTransient<RoleManager<AppRole>, RoleManager<AppRole>>();
+            services.AddTransient<IUserService, UserService>();
 
             services.AddControllersWithViews();
 
